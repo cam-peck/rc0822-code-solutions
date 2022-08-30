58737d07a8293b26e0f469990a10d6d1d8b72849
Cameron <cameronpeck317@gmail.com>
2022-08-30 17:06:13 +0000

    complete js-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,105 @@
+var orderHistory = [
+  {
+    shipTo: 'JS Masher',
+    orderDetails: {
+      totalPrice: 34.00,
+      dateOrderPlaced: '08-04-2020',
+      dateOrderDelivered: '08-08-2020',
+      deliveryDetails: 'handed directly to resident',
+      dateReturnCloses: '09-07-2020',
+      orderNumber: '114-3941689-8772232'
+    },
+    productDetails: [{
+      itemNumber: '1 / 1',
+      productPrice: 31.55,
+      productImg: 'image-placeholder',
+      productType: 'print',
+      printData: {
+        printType: 'book',
+        printTitle: 'JavaScript for Impatient Programmers',
+        printAuthor: 'Raushmayer, Dr. Axel'
+      }
+    }]
+  },
+
+  {
+    shipTo: 'JS Masher',
+    orderDetails: {
+      totalPrice: 44.53,
+      dateOrderPlaced: '07-19-2020',
+      dateOrderDelivered: '07-20-2020',
+      deliveryDetails: 'handed directly to resident',
+      dateReturnCloses: '08-19-2020',
+      orderNumber: '113-9984268-1280257'
+    },
+    productDetails: [{
+      itemNumber: '1 / 1',
+      productPrice: 41.33,
+      productImg: 'image-placeholder',
+      productType: 'print',
+      printData: {
+        printType: 'book',
+        printTitle: 'The Timeless Way of Building',
+        printAuthor: 'Alexander, Christopher'
+      }
+    }]
+  },
+
+  {
+    shipTo: 'JS Masher',
+    orderDetails: {
+      totalPrice: 17.22,
+      dateOrderPlaced: '07-04-2020',
+      dateOrderDelivered: '07-07-2020',
+      deliveryDetails: 'handed directly to resident',
+      dateReturnCloses: '08-05-2020',
+      orderNumber: '114-2885557-9059409'
+    },
+    productDetails: [{
+      itemNumber: '1 / 1',
+      productPrice: 15.98,
+      productImg: 'image-placeholder',
+      productType: 'electronics',
+      electronicsData: {
+        electronicsType: 'Video Games',
+        itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch...'
+      }
+    }]
+  },
+
+  {
+    shipTo: 'JS Masher',
+
+    orderDetails: {
+      totalPrice: 138.93,
+      dateOrderPlaced: '06-03-2020',
+      dateOrderDelivered: '06-05-2020',
+      deliveryDetails: null,
+      dateReturnCloses: '08-04-2022',
+      orderNumber: '113-2883177-2648248'
+    },
+
+    productDetails: [{
+      itemNumber: '1 / 2',
+      productPrice: 94.95,
+      productImg: 'image-placeholder',
+      productType: 'electronics',
+      electronicsData: {
+        electronicsType: 'Video Games',
+        itemName: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)'
+      }
+    },
+
+    {
+      itemNumber: '2 / 2',
+      productPrice: 33.99,
+      productImg: 'image-placeholder',
+      productType: 'print',
+      printData: {
+        printType: 'book',
+        printTitle: 'The Art of Sql',
+        printAuthor: 'Faroult, Stephane'
+      }
+    }]
+  }
+];
