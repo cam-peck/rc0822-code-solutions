51a0f4fba189be3809d09f28c79b0329b1340cf6
Cameron <cameronpeck317@gmail.com>
2022-09-04 18:56:41 +0000

    complete dom-query-exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var $heading = document.querySelector('h1');
+console.log($heading);
+console.dir($heading);
+
+var $explanation = document.querySelector('#explanation');
+console.log($explanation);
+console.dir($explanation);
+
+var $hint = document.querySelector('.hint');
+console.log($hint);
+console.dir($hint);
+
+var $allParaElements = document.querySelectorAll('p');
+console.log($allParaElements);
+
+var $allLinks = document.querySelectorAll('.example-link');
+console.log($allLinks);
