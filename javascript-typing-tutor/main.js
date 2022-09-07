26e8afdae87e9e389d41cf690c72056a670fadc7
Cameron <cameronpeck317@gmail.com>
2022-09-07 01:33:52 +0000

    finish main.js typing functionality

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,46 @@
+// Set-up HTML document with sentence
+
+var $sentenceHolder = document.querySelector('.container p');
+
+var pangrams = ['The quick brown fox jumps over a lazy dog.', 'Waltz, nymph, for quick jigs vex Bud', 'Sphinx of black quartz, judge my vow.', 'Pack my box with five dozen liquor jugs.', 'Glib jocks quiz nymph to vex dwarf.', 'The five boxing wizards jump quickly.', 'How vexingly quick daft zebras jump.', 'Two driven jocks help fax my big quiz.', 'A wizard\'s job is to vex chumps quickly in fog.', 'Watch "Jeopardy!", Alex Trebek\'s fun TV quiz game.'];
+var currentSentence = getSentence();
+
+function getSentence() {
+  return pangrams[Math.floor(Math.random() * pangrams.length)];
+}
+
+for (let i = 0; i < currentSentence.length; i++) {
+  var node = document.createElement('span');
+  node.textContent = `${currentSentence[i]}`;
+  $sentenceHolder.appendChild(node);
+}
+
+getSentence();
+
+// User-typing interactivity
+
+var $sentenceNodes = document.querySelectorAll('span');
+var $perfectScore = document.querySelector('.perfect-score');
+// var $currentScore = document.querySelector('.current-score');
+// var incorrectCounter = 0;
+var perfectScore = $sentenceNodes.length - 1;
+
+$perfectScore.textContent = `Perfect Score: ${perfectScore}`;
+$sentenceNodes[0].classList.toggle('underline'); // initial underline
+var i = 0;
+
+document.addEventListener('keydown', function (event) {
+  if (i >= perfectScore) {
+    // var finalScore = Math.floor(((perfectScore - incorrectCounter) / perfectScore) * 100);
+    // console.log('You finished! You were', finalScore, '% accurate!');
+  }
+  if (event.key === $sentenceNodes[i].textContent) {
+    $sentenceNodes[i].classList.toggle('underline');
+    $sentenceNodes[i].className = ('success');
+    i++;
+    $sentenceNodes[i].classList.toggle('underline');
+  } else {
+    $sentenceNodes[i].className = 'failure underline';
+    // incorrectCounter++;
+  }
+});
