a5c17b1f513163384c6859a55d12bf13fb8cae65
Cameron <cameronpeck317@gmail.com>
2022-09-08 18:39:58 +0000

    add capitalize and lowercase functions and first letter / later letter check

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,9 +1,11 @@
 /* exported titleCase */
+// weird colon case needs fixed... (REEE!)
 function titleCase(title) {
   var splitTitle = title.split(' ');
   var output = '';
   // var colon = false;
   var acronyms = ['API'];
+  var lowercaseWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
   var specialWords = {
     javascript: 'JavaScript'
   };
@@ -17,9 +19,39 @@ function titleCase(title) {
     if (acronyms.includes(splitTitle[i].toUpperCase())) { // check acronyms array
       output += splitTitle[i].toUpperCase(); // if includes, add all caps to result
     }
+    if (i === 0 && !Object.keys(specialWords).includes(splitTitle[i].toLowerCase()) && !acronyms.includes(splitTitle[i].toUpperCase())) { // capitalize first letter if other two cases are false AND i === 0
+      output += capitalizeWord(splitTitle[i]); // capitalize first word
+    }
+    if (i !== 0 && !Object.keys(specialWords).includes(splitTitle[i].toLowerCase()) && !acronyms.includes(splitTitle[i].toUpperCase())) {
+      if (splitTitle[i].length > 3) { // capitalize all words longer than 3 char
+        output += capitalizeWord(splitTitle[i]);
+      } else if (lowercaseWords.includes(splitTitle[i].toLowerCase())) {
+        output += lowercaseWord(splitTitle[i]);
+      } else {
+        output += capitalizeWord(splitTitle[i]);
+      }
+    }
+    if (i !== splitTitle.length - 1) { output += ' '; }
   }
   // console.log(output);
   return output;
+}
+
+function capitalizeWord(word) {
+  var currentWord = '';
+  currentWord += word[0].toUpperCase(); // capitalize first letter
+  for (let j = 1; j < word.length; j++) { // lowercase remaining letters
+    currentWord += word[j].toLowerCase();
+  }
+  return currentWord;
+}
+
+function lowercaseWord(word) {
+  var currentWord = '';
+  for (let j = 0; j < word.length; j++) { // lowercase remaining letters
+    currentWord += word[j].toLowerCase();
+  }
+  return currentWord;
 }
 // oh my...
 
