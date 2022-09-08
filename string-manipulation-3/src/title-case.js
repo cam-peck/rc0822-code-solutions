2065b219901ac168e55ba1823dc845e3c8820b66
Cameron <cameronpeck317@gmail.com>
2022-09-08 22:05:18 +0000

    complete string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -3,7 +3,8 @@
 function titleCase(title) {
   var splitTitle = title.split(' ');
   var output = '';
-  // var colon = false;
+  var colon = false;
+  var done = false;
   var acronyms = ['API'];
   var lowercaseWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
   var specialWords = {
@@ -11,29 +12,47 @@ function titleCase(title) {
   };
 
   for (let i = 0; i < splitTitle.length; i++) { // iterate through each word in array
+    done = false;
+    if (splitTitle[i].split('-').length === 2) { // check for hyphens
+      output += capitalizeHyphen(splitTitle[i]);
+      done = true;
+    }
+    if (colon === true) {
+      output += capitalizeWord(splitTitle[i]);
+      colon = false;
+      done = true;
+    }
+    if (splitTitle[i].split(':').length === 2) { // if a colon was present in last word, it's treated here
+      splitTitle[i] = splitTitle[i].split(':')[0]; // remove colon from current word so that it can continue down the chain normally
+      colon = true; // // set colon variable to true for adding colon later
+    }
     for (const key in specialWords) { // check specialwords object for weird cases
       if (splitTitle[i].toLowerCase() === key) {
         output += specialWords[key]; // add correctly cased example to output
+        done = true;
       }
     }
     if (acronyms.includes(splitTitle[i].toUpperCase())) { // check acronyms array
       output += splitTitle[i].toUpperCase(); // if includes, add all caps to result
+      done = true;
     }
-    if (i === 0 && !Object.keys(specialWords).includes(splitTitle[i].toLowerCase()) && !acronyms.includes(splitTitle[i].toUpperCase())) { // capitalize first letter if other two cases are false AND i === 0
+    if (i === 0 && done === false) { // capitalize first letter if other two cases are false AND i === 0
       output += capitalizeWord(splitTitle[i]); // capitalize first word
     }
-    if (i !== 0 && !Object.keys(specialWords).includes(splitTitle[i].toLowerCase()) && !acronyms.includes(splitTitle[i].toUpperCase())) {
+    if (i !== 0 && done === false) {
       if (splitTitle[i].length > 3) { // capitalize all words longer than 3 char
         output += capitalizeWord(splitTitle[i]);
-      } else if (lowercaseWords.includes(splitTitle[i].toLowerCase())) {
+      } else if (lowercaseWords.includes(splitTitle[i].toLowerCase())) { // lowercase all prepositions, articles, and conjunctions
         output += lowercaseWord(splitTitle[i]);
       } else {
-        output += capitalizeWord(splitTitle[i]);
+        output += capitalizeWord(splitTitle[i]); // else give it the capitalize treatment
       }
     }
-    if (i !== splitTitle.length - 1) { output += ' '; }
+    if (colon === true) {
+      output += ':';
+    }
+    if (i !== splitTitle.length - 1) { output += ' '; } // add space to all words except for last
   }
-  // console.log(output);
   return output;
 }
 
@@ -52,6 +71,14 @@ function lowercaseWord(word) {
     currentWord += word[j].toLowerCase();
   }
   return currentWord;
+}
+
+function capitalizeHyphen(word) {
+  var splitByHyphen = word.split('-');
+  for (let k = 0; k < splitByHyphen.length; k++) {
+    splitByHyphen[k] = capitalizeWord(splitByHyphen[k]);
+  }
+  return splitByHyphen.join('-');
 }
 // oh my...
 
