250ea6c1539508765258ab263651c31a23a34eeb
Cameron <cameronpeck317@gmail.com>
2022-09-09 17:15:40 +0000

    start pseudo-code for card game

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,11 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+// create four player obects, each player has name: 'name-here' and hand: 'cards-here' data.
+// create array of 52 objects, each object needs a rank and suit
+// we're lazy, so we're going to use JS to create the deck for us
+// declare an empty array to store our object in named 'deck'
+// declare an array of all four suits named 'suits'
+// declare an array of all values from Ace --> King named 'values'
+// iterate through suits first, and values second.
+// at each element of values, push an object with the suit and value to the deck
+// when finished iterating, should have deck of 52 cards.
