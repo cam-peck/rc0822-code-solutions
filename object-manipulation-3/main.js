fc8c3ed0226e2948eecf9cdff7bf1a75879f7c7a
Cameron <cameronpeck317@gmail.com>
2022-09-09 21:30:24 +0000

    add shuffle method

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,5 +1,28 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
+var players = [];
+var playerNames = ['Harry', 'Ron', 'Hermoine', 'Dumblydore'];
+for (let i = 0; i < playerNames.length; i++) {
+  var currentPlayer = {};
+  currentPlayer.name = playerNames[i];
+  currentPlayer.hand = [];
+  players.push(currentPlayer);
+}
+
+var deck = [];
+var suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+var values = ['Ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'Jack', 'Queen', 'King'];
+for (let i = 0; i < suits.length; i++) {
+  for (let j = 0; j < values.length; j++) {
+    var currentCard = {};
+    currentCard.suit = suits[i];
+    currentCard.value = values[j];
+    deck.push(currentCard);
+  }
+}
+
+var shuffledDeck = _.shuffle(deck);
+console.log(shuffledDeck);
 // create four player obects, each player has name: 'name-here' and hand: 'cards-here' data.
 // create array of 52 objects, each object needs a rank and suit
 // we're lazy, so we're going to use JS to create the deck for us
