6a6056befab678c61d656f775e0227fcb5b7260b
Cameron <cameronpeck317@gmail.com>
2022-09-11 14:27:04 +0000

    finish object-manipulation-3 exercise

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,6 +1,11 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
 function createPlayers(playerNames) {
+  // creates and returns player objects with name and hand properties, takes string or array //
+  if (typeof playerNames === 'string') {
+    var withoutCommas = playerNames.replaceAll(',', '');
+    playerNames = withoutCommas.split(' ');
+  }
   var players = [];
   for (let i = 0; i < playerNames.length; i++) { // create players
     var currentPlayer = {};
@@ -12,6 +17,7 @@ function createPlayers(playerNames) {
 }
 
 function createDeck() {
+  // creates and returns a standard 52 card deck //
   var deck = [];
   var suits = ['clubs', 'spades', 'hearts', 'diamonds'];
   var values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
@@ -27,10 +33,12 @@ function createDeck() {
 }
 
 function shuffleDeck(deck) {
+  // shuffles an input array, takes an array as input //
   return _.shuffle(deck);
 }
 
 function dealCards(players, cardsToDeal, deck) {
+  // deals cards to players by assigning cards to their hand property //
   for (let i = 0; i < players.length; i++) {
     var cards = [];
     for (let i = 0; i < cardsToDeal; i++) {
@@ -42,6 +50,7 @@ function dealCards(players, cardsToDeal, deck) {
 }
 
 function replaceFaceCards(players) {
+  // replaces face cards with numeric values in player hand //
   var faceCards = {
     Ace: 11,
     Jack: 10,
@@ -60,7 +69,8 @@ function replaceFaceCards(players) {
 }
 
 function scoreRound(players) {
-  var currentWinner = '';
+  // scores a round and breaks ties, logs the winning player to the console //
+  var currentWinners = [];
   var currentHighScore = 0;
   for (let i = 0; i < players.length; i++) {
     var currentScore = 0;
@@ -69,15 +79,22 @@ function scoreRound(players) {
     }
     console.log('Player Name: ', players[i].name);
     console.log('  Player Score: ', currentScore);
-    if (currentScore > currentHighScore) {
-      currentWinner = players[i].name;
+    if (currentScore > currentHighScore) { // new highscore
+      currentWinners = [players[i].name]; // replace old winners with new winner
       currentHighScore = currentScore;
+    } else if (currentScore === currentHighScore) { // add tiebreaker winners
+      currentWinners.push(players[i].name);
     }
   }
-  return currentWinner;
+  if (currentWinners.length === 1) {
+    console.log('The winner is', currentWinners[0], '!');
+  } else {
+    play(currentWinners);
+  }
+
 }
 
-var playerNames = ['Harry', 'Ron', 'Hermoine', 'Dumblydore'];
+const playerNames = prompt('Enter the player names, seperated by commas:', 'Harry, Ron, Hermoine, Dumblydore'); // name entry
 
 function play(playerNames) {
   var players = createPlayers(playerNames);
@@ -85,11 +102,11 @@ function play(playerNames) {
   var shuffledDeck = shuffleDeck(deck);
   dealCards(players, 2, shuffledDeck);
   replaceFaceCards(players);
-  var winner = scoreRound(players);
-  console.log('The winner is', winner, '!');
+  scoreRound(players);
 }
 
 play(playerNames);
+
 // create four player obects, each player has name: 'name-here' and hand: 'cards-here' data.
 // create array of 52 objects, each object needs a rank and suit
 // we're lazy, so we're going to use JS to create the deck for us
@@ -99,3 +116,7 @@ play(playerNames);
 // iterate through suits first, and values second.
 // at each element of values, push an object with the suit and value to the deck
 // when finished iterating, should have deck of 52 cards.
+// shuffle deck using lodash
+// assign cards to player hands off the top of the deck -- make sure to shift them off so no doubles
+// replace face cards with numeric values
+// find the winner --> if more than one, play again
