e9533571835cf38a31ebc74e332d3fb24789b3f6
Cameron <cameronpeck317@gmail.com>
2022-09-22 19:21:24 +0000

    complete javascript-ajax exercise

---
+++ javascript-ajax/users.js
@@ -0,0 +1,20 @@
+var $userList = document.querySelector('#user-list');
+console.log($userList);
+
+function getUserData() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function (event) {
+    console.log(xhr.status);
+    console.log(xhr.response);
+    for (let i = 0; i < xhr.response.length; i++) {
+      var $newUserLi = document.createElement('li');
+      $newUserLi.textContent = xhr.response[i].name;
+      $userList.appendChild($newUserLi);
+    }
+  });
+  xhr.send();
+}
+
+getUserData();
