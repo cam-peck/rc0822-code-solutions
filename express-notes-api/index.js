94231a55f8259481badfee62e5584d43637a07cb
Cameron <cameronpeck317@gmail.com>
2022-10-18 22:56:08 +0000

    add two helper functions, overwriteJSON & handleParamErrors to remove repeated code

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,9 +1,13 @@
+// Global Variables & Imports //
+
 const express = require('express');
 const app = express();
 const data = require('./data.json');
 const fs = require('fs');
 
 app.use(express.json());
+
+// Paths //
 
 app.get('/api/notes', (req, res) => { // GET requests for all notes
   const dataArray = [];
@@ -13,77 +17,44 @@ app.get('/api/notes', (req, res) => { // GET requests for all notes
   res.json(dataArray);
 });
 
-app.get('/api/notes/:noteId', (req, res) => { // GET requests for notes with a specific ID
-  if (data.notes[req.params.noteId]) { // id does exist
+app.get('/api/notes/:noteId', (req, res) => {
+  if (data.notes[req.params.noteId]) {
     res.send(data.notes[req.params.noteId]);
-  } else if (!data.notes[req.params.noteId] && isNaN(req.params.noteId)) { // id is not an integer
-    res.status(400).send(`Error: Your id, ${req.params.noteId}, is not a positive integer.`);
-  } else { // id is an integer, but does not exist
-    res.status(404).send(`Error: Your id, ${req.params.noteId}, does not exist.`);
+  } else {
+    handleParamErrors(req, res);
   }
 });
 
-app.post('/api/notes', (req, res) => { // POST requests for a new note
+app.post('/api/notes', (req, res) => {
   if (!req.body.content) {
     res.status(400).send('Error: No content attached. Ensure your key name is "content", followed by your desired value.');
   } else {
     data.notes[data.nextId] = req.body;
     data.notes[data.nextId].id = data.nextId;
     data.nextId++;
-    const newJSON = JSON.stringify(data, null, 2);
-    fs.writeFile('data.json', newJSON, 'utf8', err => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.error(err);
-        res.status(500).send('Error: An unexpected error occured.');
-      } else {
-        res.status(201).send(req.body);
-      }
-    });
+    overwriteJSON(data, res);
   }
 });
 
-app.delete('/api/notes/:noteId', (req, res) => { // DELETE requests for a note by ID
-  if (data.notes[req.params.noteId]) { // id does exist
+app.delete('/api/notes/:noteId', (req, res) => {
+  if (data.notes[req.params.noteId]) {
     delete data.notes[req.params.noteId];
-    const newJSON = JSON.stringify(data, null, 2);
-    fs.writeFile('data.json', newJSON, 'utf8', err => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.error(err);
-        res.status(500).send('Error: An unexpected error occured.');
-      } else {
-        res.sendStatus(204);
-      }
-    });
-  } else if (!data.notes[req.params.noteId] && isNaN(req.params.noteId)) { // id is not an integer
-    res.status(400).send(`Error: Your id, ${req.params.noteId}, is not a positive integer.`);
-  } else { // id is an integer, but does not exist
-    res.status(404).send(`Error: Your id, ${req.params.noteId}, does not exist.`);
+    overwriteJSON(data, res);
+  } else {
+    handleParamErrors(req, res);
   }
 });
 
 app.put('/api/notes/:noteId', (req, res) => {
-  if (data.notes[req.params.noteId]) { // id does exist
+  if (data.notes[req.params.noteId]) {
     if (req.body.content) {
       data.notes[req.params.noteId].content = req.body.content;
-      const newJSON = JSON.stringify(data, null, 2);
-      fs.writeFile('data.json', newJSON, 'utf8', err => {
-        if (err) {
-          // eslint-disable-next-line no-console
-          console.error(err);
-          res.status(500).send('Error: An unexpected error occured.');
-        } else {
-          res.sendStatus(204);
-        }
-      });
+      overwriteJSON(data, res);
     } else {
       res.status(400).send('Error: No content attached. Ensure your key name is "content", followed by your desired value.');
     }
-  } else if (!data.notes[req.params.noteId] && isNaN(req.params.noteId)) { // id is not an integer
-    res.status(400).send(`Error: Your id, ${req.params.noteId}, is not a positive integer.`);
-  } else { // id is an integer, but does not exist
-    res.status(404).send(`Error: Your id, ${req.params.noteId}, does not exist.`);
+  } else {
+    handleParamErrors(req, res);
   }
 });
 
@@ -91,3 +62,26 @@ app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on port 3000');
 });
+
+// Helper Functions //
+
+function overwriteJSON(data, res) { // takes data to overwrite current data.json file with and rewrites the file
+  const newJSON = JSON.stringify(data, null, 2);
+  fs.writeFile('data.json', newJSON, 'utf8', err => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      res.status(500).send('Error: An unexpected error occured.');
+    } else {
+      res.sendStatus(204);
+    }
+  });
+}
+
+function handleParamErrors(req, res) { // handles errors in passed in parameters by the user
+  if (!data.notes[req.params.noteId] && isNaN(req.params.noteId)) { // id is not an integer
+    res.status(400).send(`Error: Your id, ${req.params.noteId}, is not a positive integer.`);
+  } else { // id is an integer, but does not exist
+    res.status(404).send(`Error: Your id, ${req.params.noteId}, does not exist.`);
+  }
+}
