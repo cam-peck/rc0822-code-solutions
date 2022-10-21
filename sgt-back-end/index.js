0e3f025193ac1adcb724aea58b924e2a3e5eacc1
Cameron <cameronpeck317@gmail.com>
2022-10-21 23:30:58 +0000

    complete DELETE route and functionality

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -65,7 +65,7 @@ app.put('/api/grades/:gradeId', (req, res, next) => {
     res.status(400).send('Error: Invalid score --> valid scores are integers between 0 and 100.');
     return;
   }
-  if (!gradeReplaceId || isNaN(Number(gradeReplaceId))) {
+  if (!gradeReplaceId || isNaN(Number(gradeReplaceId))) { // gradeId is missing or not a number
     res.status(400).send('Error: Either you did not specify a gradeId to replace, or the gradeId is not a valid integer.');
   }
   const sql = `
@@ -87,6 +87,28 @@ app.put('/api/grades/:gradeId', (req, res, next) => {
   }).catch(err => {
     console.error(err);
     res.status(500).send('Error: An unexpected error occured');
+  });
+});
+
+app.delete('/api/grades/:gradeId', (req, res, next) => {
+  const gradeDeleteId = req.params.gradeId;
+  if (!gradeDeleteId || isNaN(Number(gradeDeleteId))) { // gradeId is missing or not a number
+    res.status(400).send('Error: Either you did not specify a gradeId to delete, or the gradeId is not a valid integer.');
+  }
+  const sql = `
+  DELETE
+    FROM "grades"
+   WHERE "gradeId" = $1
+   RETURNING *;
+  `;
+  const params = [gradeDeleteId];
+  db.query(sql, params).then(result => {
+    const deletedRow = result.rows[0];
+    if (!deletedRow) {
+      res.status(404).send(`Error: Cannot find grade with "gradeId" ${gradeDeleteId}.`);
+    } else {
+      res.sendStatus(204);
+    }
   });
 });
 
