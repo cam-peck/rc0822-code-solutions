f38cfd558e76e85482c7cb7e410da0eba5d414d4
Cameron <cameronpeck317@gmail.com>
2022-10-21 23:22:50 +0000

    complete PUT route and functionality

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -20,8 +20,7 @@ app.get('/api/grades', (req, res, next) => {
     const gradeData = result.rows;
     res.json(gradeData);
   }).catch(err => { // query failed, promise rejected
-    // eslint-disable-next-line no-console
-    console.err(err);
+    console.error(err);
     res.status(500).send('Error: An unexpected error occured');
   });
 });
@@ -48,8 +47,45 @@ app.post('/api/grades', (req, res, next) => {
     const newEntry = result.rows[0];
     res.status(201).send(newEntry);
   }).catch(err => {
-    // eslint-disable-next-line no-console
-    console.err(err);
+    console.error(err);
+    res.status(500).send('Error: An unexpected error occured');
+  });
+});
+
+app.put('/api/grades/:gradeId', (req, res, next) => {
+  const studentName = req.body.name;
+  const studentCourse = req.body.course;
+  const studentScore = req.body.score;
+  const gradeReplaceId = req.params.gradeId;
+  if (!studentName || !studentCourse || !studentScore) { // parameters are missing from the input
+    res.status(400).send('Error: Be sure to include name, course, score in your request header.');
+    return;
+  }
+  if (isNaN(Number(studentScore)) || studentScore < 0 || studentScore > 100) { // grade value is invalid in input
+    res.status(400).send('Error: Invalid score --> valid scores are integers between 0 and 100.');
+    return;
+  }
+  if (!gradeReplaceId || isNaN(Number(gradeReplaceId))) {
+    res.status(400).send('Error: Either you did not specify a gradeId to replace, or the gradeId is not a valid integer.');
+  }
+  const sql = `
+  UPDATE "grades"
+  SET    "name"    =  $1,
+         "course"  =  $2,
+         "score"   =  $3
+  WHERE  "gradeId" =  $4
+  RETURNING *;
+  `;
+  const params = [studentName, studentCourse, studentScore, gradeReplaceId];
+  db.query(sql, params).then(result => {
+    const editedRow = result.rows[0];
+    if (!editedRow) {
+      res.status(404).send(`Error: Cannot find grade with "gradeId" ${gradeReplaceId}.`);
+    } else {
+      res.send(editedRow);
+    }
+  }).catch(err => {
+    console.error(err);
     res.status(500).send('Error: An unexpected error occured');
   });
 });
