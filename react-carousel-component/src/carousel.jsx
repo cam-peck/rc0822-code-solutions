fe050fe283b90ea17e0852626ae6e279d26ca6e4
Cameron <cameronpeck317@gmail.com>
2022-10-31 16:26:11 +0000

    complete react-carousel-component challenge

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -4,15 +4,28 @@ export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentImageIndex: Math.floor(images.length / 2)
+      currentImageIndex: Math.floor(this.props.imageArray.length / 2)
     };
-    this.handleForwardClick = this.handleForwardClick.bind(this);
-    this.handleBackwardClick = this.handleBackwardClick.bind(this);
+    this.moveForward = this.moveForward.bind(this);
+    this.moveBackward = this.moveBackward.bind(this);
     this.handleCircleClick = this.handleCircleClick.bind(this);
   }
 
-  handleForwardClick() {
-    if (this.state.currentImageIndex !== images.length - 1) {
+  startCarousel() { // starts carousel --> moves every 3 seconds
+    this.interval = setInterval(() => this.moveForward(), 3000);
+  }
+
+  stopCarousel() { // stops carousel
+    clearInterval(this.interval);
+  }
+
+  componentDidMount() { // starts carousel on screen load
+    this.startCarousel();
+  }
+
+  moveForward() { // advances carousel forward one step
+    this.stopCarousel();
+    if (this.state.currentImageIndex !== this.props.imageArray.length - 1) {
       this.setState({
         currentImageIndex: this.state.currentImageIndex + 1
       });
@@ -21,71 +34,81 @@ export default class Carousel extends React.Component {
         currentImageIndex: 0
       });
     }
+    this.startCarousel();
   }
 
-  handleBackwardClick() {
+  moveBackward() { // moves carousel back one step
+    this.stopCarousel();
     if (this.state.currentImageIndex !== 0) {
       this.setState({
         currentImageIndex: this.state.currentImageIndex - 1
       });
     } else {
       this.setState({
-        currentImageIndex: images.length - 1
+        currentImageIndex: this.props.imageArray.length - 1
       });
     }
+    this.startCarousel();
   }
 
-  handleCircleClick() {
-
+  handleCircleClick(circleIndex) { // moves carousel to clicked circle
+    this.stopCarousel();
+    this.setState({
+      currentImageIndex: circleIndex
+    });
+    this.startCarousel();
   }
 
   render() {
     const activeImageIndex = this.state.currentImageIndex;
     return (
       <div className="container">
-        <i className="fa-solid fa-angle-left fa-xl" onClick={this.handleBackwardClick}></i>
+        <i className="fa-solid fa-angle-left fa-xl" onClick={this.moveBackward}></i>
         <div className="image-wrapper">
-          < ImageList imageArray={images} activeImage={activeImageIndex}/>
-          < CircleList imageArray={images} activeCircle={activeImageIndex} />
+          < ImageItem imageArray={this.props.imageArray} activeImage={activeImageIndex}/>
+          < CircleList imageArray={this.props.imageArray} activeCircle={activeImageIndex} onCircleChange={this.handleCircleClick}/>
         </div>
-        <i className="fa-solid fa-angle-right fa-xl" onClick={this.handleForwardClick}></i>
+        <i className="fa-solid fa-angle-right fa-xl" onClick={this.moveForward}></i>
       </div>
-    );
-  }
-}
-
-class ImageList extends React.Component {
-  render() {
-    const imageArray = this.props.imageArray;
-    const activeImageIndex = this.props.activeImage;
-    const itemToRender = imageArray[activeImageIndex];
-    return (
-      <ImageItem key={itemToRender.id.toString()}
-                imageUrl={itemToRender.url} />
     );
   }
 }
 
 class ImageItem extends React.Component {
   render() {
+    const imageArray = this.props.imageArray;
+    const activeImageIndex = this.props.activeImage;
+    const itemToRender = imageArray[activeImageIndex];
     return (
       <div>
-        <img src={this.props.imageUrl} alt="pokemon-img" className="carousel-image image-active"/>
+        <img src={itemToRender.url} alt="pokemon-img" className="carousel-image image-active"/>
       </div>
     );
   }
 }
 
 class CircleList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) { // pass circle clicks back up to carousel for handling (and state management)
+    const circleIndex = Number(event.target.getAttribute(['circleindex']));
+    this.props.onCircleChange(circleIndex);
+  }
+
   render() {
     const imageArray = this.props.imageArray;
     const activeCircleIndex = this.props.activeCircle;
     const circleItems = imageArray.map((image, index) => {
+      let isActiveBool;
       if (activeCircleIndex === index) {
-        return < CircleItem key={image.id.toString()} active={true} />;
+        isActiveBool = true;
       } else {
-        return < CircleItem key={image.id.toString()} active={false} />;
+        isActiveBool = false;
       }
+      return < CircleItem key={image.id.toString()} circleIndex={index} active={isActiveBool} onClick={this.handleChange} />;
     });
     return (
       <div className="selectors">
@@ -97,37 +120,14 @@ class CircleList extends React.Component {
 
 class CircleItem extends React.Component {
   render() {
-    let circleState;
+    let circleType;
     if (this.props.active) {
-      circleState = 'fa-solid';
+      circleType = 'fa-solid';
     } else {
-      circleState = 'fa-regular';
+      circleType = 'fa-regular';
     }
     return (
-      <i className={`${circleState} fa-circle`}></i>
+      <i className={`${circleType} fa-circle`} circleindex={this.props.circleIndex} onClick={this.props.onClick}></i>
     );
   }
 }
-
-const images = [
-  {
-    id: '001',
-    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
-  },
-  {
-    id: '004',
-    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'
-  },
-  {
-    id: '007',
-    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
-  },
-  {
-    id: '025',
-    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'
-  },
-  {
-    id: '030',
-    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/030.png'
-  }
-];
